223a0c6b68730a1443f7fa27e9add8b4b51966e1
Florencia Valladares <fvalladares1@uc.cl>
2019-10-11 12:15:46 -0300

    add persist with page reload

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store/configureStore';
+import { store, persistor } from './store/configureStore';
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -1,9 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const configureStore = (initialState) => createStore(
-  rootReducer,
+  persistedReducer,
   initialState,
   applyMiddleware(thunk)
 );
@@ -16,4 +26,6 @@ const initialState = {
 
 const store = configureStore(initialState);
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
