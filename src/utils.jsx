c5421339c660e8aa0c48d0c069f6f322980b7f93
Florencia Valladares <fvalladares1@uc.cl>
2019-10-14 16:46:30 -0300

    add post url to auth

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,9 +1,21 @@
+const AUTH_URL = 'http://localhost:3000/api/v1';
 
 function sleep(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
+const postAuth = (route, body) => (
+  fetch(`${AUTH_URL}${route}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+);
+
 export {
-  // eslint-disable-next-line import/prefer-default-export
   sleep,
+  postAuth,
 };
